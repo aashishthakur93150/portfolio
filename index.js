817091d3bf1fbfe5055ef032c6cb775e1d0854df
pradeep_refer <pradeepkumar@referloan.in>
2024-05-02 17:35:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 400, 3000);
+    counter("count2", 100, 500, 2500);
+    counter("count3", 0, 40, 3000);
+    counter("count4",10, 50, 3000);
+   });
+//    ------------------feedback----------------------------
+
+
+const next=document.querySelector('#next')
+const prev=document.querySelector('#prev')
+
+function handleScrollNext (direction) {
+  const cards = document.querySelector('.card-content')
+  cards.scrollLeft=cards.scrollLeft += window.innerWidth / 2 > 600 ? window.innerWidth /2 : window.innerWidth -100
+}
+
+function handleScrollPrev (direction) {
+  const cards = document.querySelector('.card-content')
+  cards.scrollLeft=cards.scrollLeft -= window.innerWidth / 2 > 600 ? window.innerWidth /2 : window.innerWidth -100
+}
+
+next.addEventListener('click', handleScrollNext)
+prev.addEventListener('click', handleScrollPrev)
+
+// -------------clint logos------------------
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
+
